1cdb8998b296e199bca0b926f130263853935753
zhouqi <javazhouqi@163.com>
2021-12-14 22:09:18 +0800

    gateway完善

---
+++ pxd-gateway/src/main/java/com/pxd/gateway/GatewayApp.java
@@ -0,0 +1,18 @@
+package com.pxd.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+
+}
+
