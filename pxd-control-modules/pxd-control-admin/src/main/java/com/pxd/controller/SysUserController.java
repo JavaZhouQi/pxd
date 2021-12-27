a4a07d6934c1fee50e7e358f70278490519c9480
zhouqi <javazhouqi@163.com>
2021-12-27 15:11:30 +0800

    参数校验

--- pxd-control-modules/pxd-control-admin/src/main/java/com/pxd/controller/SysUserController.java
+++ pxd-control-modules/pxd-control-admin/src/main/java/com/pxd/controller/SysUserController.java
@@ -1,15 +1,14 @@
 package com.pxd.controller;
 
 import com.pxd.control.common.result.Result;
-import com.pxd.dubbo.user.service.SysUserService;
 import com.pxd.service.ControlSysUserService;
 import com.pxd.vo.SysUserVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 @Api(tags = "登录相关接口")
 @RestController
@@ -21,7 +20,7 @@ public class SysUserController {
 
     @ApiOperation(value = "添加用户")
     @PostMapping
-    public Result<?> add(@RequestBody SysUserVo sysUserVo){
+    public Result<?> add(@Valid @RequestBody SysUserVo sysUserVo){
         return controlSysUserService.add(sysUserVo);
     }
 
