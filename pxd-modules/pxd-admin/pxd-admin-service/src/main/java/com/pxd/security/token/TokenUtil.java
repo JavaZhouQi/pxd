d1453259db4e0061a1828084a4bede51ad7c1d84
javazhouqi <javazhouqi@outlook.com>
2022-02-07 13:27:55 +0800

    增加项目简介

--- pxd-modules/pxd-admin/pxd-admin-service/src/main/java/com/pxd/security/token/TokenUtil.java
+++ pxd-modules/pxd-admin/pxd-admin-service/src/main/java/com/pxd/security/token/TokenUtil.java
@@ -14,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class TokenUtil {
+
     private static final String REDIS_TOKEN_KEY = "token:";
     private static final String REDIS_USER_KEY = "user:";
 
